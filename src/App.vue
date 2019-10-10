4e3c23bd93359e834c4731bae1a2be732e1c047d
Anaïs TAN <anais.tan.lhernould@gmail.com>
2019-10-10 11:57:36 +0200

    charge all file

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="app">
+        <button @click="addDiv()" class="style_button">
+            Cliquez ici pour ajouter une DIV
+        </button>
+        <mydiv v-drag v-for="n in range" :key="n">
+        </mydiv>
+    </div>
+</template>
+
+<script>
+
+/* importation de la div */
+import mydiv from './mydiv.vue'
+    export default {
+        name: 'app', 
+        components: {
+            mydiv, 
+        },
+        data() {
+            return{
+                range:0,
+            }
+        }, 
+        methods: {
+        addDiv: function(){
+            this.range += 1; 
+        }
+    },
+    }
+
+/* DRAG AND DROP */
+Vue.use(vdrag);
+import Vue from 'vue';
+import vdrag from 'v-drag';
+
+</script>
+
+<style>
+.center{
+    margin : 0 auto; 
+    text-align: center; 
+}
+
+.style_button{
+    background: rgb(1, 12, 36); 
+    color : #fff; 
+    font-size: 0.8em; 
+    padding : 10px; 
+    border : none; 
+}
+</style>
+
---
+++ src/mydiv.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="modele_div">
+        <input type="text" v-model="message"> 
+        <p> {{message}} </p> 
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return{
+                message: " Contenu de ma div ",
+            }
+        },
+        
+        
+    }
+</script>
+
+<style>
+    .modele_div{
+        background: rgb(241, 240, 240); 
+        width : 20%; 
+        height : auto; 
+        border : 0.5px solid #000; 
+        margin: 20px; 
+        padding: 20px; 
+    }
+</style>
